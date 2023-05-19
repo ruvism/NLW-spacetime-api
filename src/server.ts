fc6ed09a939b8cc6570b73d7593d2185a00bb241
ruvila <ruvila.magalhaes@creditas.com>
2023-05-18 21:15:52 -0300

    feat: create backend crud for project

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { memoriesRoutes } from "./routes/memories";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: true,
+});
+
+app.register(memoriesRoutes);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running on http://localhost:3333");
+  });
